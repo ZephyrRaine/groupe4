fc3ebecd5b8a8fe788d2b2f5de9baf927ff3ab00
The0CO <theo.guichard@lecole-ldlc.com>
2024-06-17 16:38:21 +0200

    modif sur db.php

--- db.php
+++ db.php
@@ -8,16 +8,6 @@ try {
     $dbh = new PDO('mysql:host=10.96.16.90;port=3306;dbname=groupe4', 'groupe4', 'groupe4');
     // set the PDO error mode to exception
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $sql = "SELECT * FROM table_name";
-    $stmt = $dbh->query($sql);
-
-    $rowCount = $stmt->rowCount();
-    if ($rowCount > 0) {
-        echo "Found " . $rowCount . " product(s).";
-    } else {
-        echo "No products found.";
-    }
-} catch(PDOException $e) {
-    echo $sql . "<br>" . $e->getMessage();
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
